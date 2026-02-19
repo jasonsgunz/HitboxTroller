d9bd4ce2ec0e14749548c0cdb6d96a7cfb57c16f
jasonsgunz <jasonmcmini@gmail.com>
2026-02-19 19:07:51 +0100

    UniversalAimbot V.3.1

--- UniversalAimbot.lua
+++ UniversalAimbot.lua
@@ -164,7 +164,7 @@ Instance.new("UIListLayout", DropdownFrame).HorizontalAlignment = "Center"
 
 local Title = Instance.new("TextLabel", Main)
 Title.Size = UDim2.new(1, -60, 0, 35); Title.Position = UDim2.new(0, 15, 0, 0); Title.BackgroundTransparency = 1
-Title.Text = "UniversalAimbot V23"; Title.TextColor3 = Color3.new(1, 1, 1); Title.Font = "GothamBold"; Title.TextSize = 14; Title.TextXAlignment = "Left"
+Title.Text = "UniversalAimbot"; Title.TextColor3 = Color3.new(1, 1, 1); Title.Font = "GothamBold"; Title.TextSize = 14; Title.TextXAlignment = "Left"
 
 local Close = Instance.new("TextButton", Main)
 Close.Size = UDim2.new(0, 25, 0, 25); Close.Position = UDim2.new(1, -30, 0, 5); Close.BackgroundColor3 = Color3.fromRGB(200, 50, 50); Close.Text = "X"; Close.TextColor3 = Color3.new(1, 1, 1)
